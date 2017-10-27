16b83ed54bbb34d57ee316a475da785637845a24
Michael Otis <mikelotis123@gmail.com>
2017-10-26 22:06:11 -0600

    Update infractions.js

--- js/infractions.js
+++ js/infractions.js
@@ -70,7 +70,7 @@
         var heatColors = ['#edf8e9','#bae4b3','#74c476','#31a354','#006d2c'];
         var pieColors = ['#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854'];
         var mapColors =  ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99']; 
-        var bubbleColors = mapColors.map(return d != '#fb9a99'; );
+        var bubbleColors = mapColors.map(function(d) { return d != '#fb9a99'; });
         var pieScaleColors = d3.scale.quantize().domain([0, pieColors.length - 1]).range(pieColors);
         var bubbleScaleColors = d3.scale.quantize().domain([0, bubbleColors.length - 1]).range(bubbleColors);
         var monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
